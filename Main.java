6285b4fb0b2033e4a2d105e9e7dd1199d8627995
unknown <hikmataprian@gmail.com>
2026-04-13 14:52:23 +0700

    tugas pertemuan 4

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-
+try {
         Scanner input = new Scanner(System.in);
 
         DaftarKereta daftar = new DaftarKereta();
@@ -20,12 +20,22 @@ public class Main {
             System.out.println(nomor + ". " + kereta + " - " + dataKereta.get(kereta));
             nomor++;
         }
-
-        System.out.print("Pilih Kereta (nomor): ");
-        int pilihan = input.nextInt();
-        input.nextLine();
-
-        String namaKereta = listKereta.get(pilihan - 1);
+int pilihan = 0;
+            try {
+                System.out.print("Pilih Kereta (nomor): ");
+                pilihan = input.nextInt();  // DIUBAH
+                input.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println("Input harus angka!");
+                return;
+            }
+            String namaKereta;
+            try {
+                namaKereta = listKereta.get(pilihan - 1);
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("Pilihan tidak tersedia!");
+                return;
+            }
 
         int harga = dataKereta.get(namaKereta);
 
@@ -42,18 +52,30 @@ public class Main {
         System.out.println("1. Ekonomi");
         System.out.println("2. Bisnis");
 
-        int jenis = input.nextInt();
+        int jenis = 0;  // DIUBAH
+            try {
+                jenis = input.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Input harus angka!");
+                return;
+            }
 
         Tiket tiket;
 
         if (jenis == 1) {
-            tiket = new TiketEkonomi(nama, namaKereta, harga, noHP, kode);
-        } else {
-            tiket = new TiketBisnis(nama, namaKereta, harga, noHP, kode);
-        }
+                tiket = new TiketEkonomi(nama, namaKereta, harga, noHP, kode);
+            } else if (jenis == 2) {
+                tiket = new TiketBisnis(nama, namaKereta, harga, noHP, kode);
+            } else {
+                System.out.println("Jenis tiket tidak valid!");
+                return;
+            }
 
         System.out.println("\n===== TIKET =====");
 
         ((CetakTiket) tiket).cetakInfo();
+        } catch (Exception e) {
+            System.out.println("Terjadi kesalahan: " + e.getMessage());
+        }
     }
 }
